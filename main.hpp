f81f3c1682403a5d181724b0dfaa143570eacef6
Dmitrii Sharandin <dshar@mail.ru>
2023-10-28 13:02:02 +0500

    list_double

--- main.hpp
+++ main.hpp
@@ -7,7 +7,6 @@ namespace m29hw
     template<typename T>
     void threadFunc(Mylist<T>& list)
     {
-        std::cout << "thread func\n";
         T value = 0;
         for(size_t i = 0; i < 5; ++i){
             list.insertIntoMiddle(i + value, rand() % 10);
